794f4a7a3a867001207ee36c4404dad01b908fa6
NovaShen555 <admin@novashen.top>
2026-02-06 20:42:57 +0800

    修改版本号

--- buildSrc/src/main/kotlin/Build.kt
+++ buildSrc/src/main/kotlin/Build.kt
@@ -3,8 +3,8 @@
  */
 object BuildVersion {
     const val applicationId     =       "com.novashen.riverside"
-    const val versionCode       =       10003  // 1.0.3 -> 1*10000 + 0*100 + 3 = 10003
-    const val versionName       =       "1.0.3"
+    const val versionCode       =       10005  // 1.0.3 -> 1*10000 + 0*100 + 3 = 10003
+    const val versionName       =       "1.0.5"
     const val buildToolsVersion =       "32.0.0"
     const val compileSdk        =       32
     const val minSdk            =       23
